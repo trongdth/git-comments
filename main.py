2394daef3d3521f90cecb65d8471e6f8456205cb
InNoobWeTrust <vu.truong.16193@gmail.com>
2023-02-17 01:46:47 +0700

    feat: add csv header and correct PR link

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ if __name__ == '__main__':
             print("=====> your configuration: \n {}".format(configs))
             user_token = configs['user-token']
             user_id = configs['user-id']
-            urls = configs['githubs']
+            repos = configs['githubs']
             created_at = configs['created-at']
             is_export_csv = configs['csv']
             all_prs = []
             pr_comments = {}
 
             print("=====> your pull requests:")
-            for url in urls:
+            for repo in repos:
                 prs = all_pull_requests(
-                    user_token, user_id, repos=url, created_at=created_at)
+                    user_token, user_id, repos=repo, created_at=created_at)
                 all_prs += prs
             print(all_prs)
 
@@ -31,13 +31,14 @@ if __name__ == '__main__':
             for pr in all_prs:
                 comments = all_comments(
                     user_token, pull_request=pr)
-                pr_comments[pr.url] = comments
+                pr_comments[pr.html_url] = comments
             print(pr_comments)
 
             if is_export_csv == True:
                 print("======> dump report to out.csv")
                 with open('./out.csv', 'w') as f:
                     writer = csv.writer(f)
+                    writer.writerow(['Name', 'comments'])
                     for key, values in pr_comments.items():
                         writer.writerow(
                             [key, len(pr_comments[key])])
